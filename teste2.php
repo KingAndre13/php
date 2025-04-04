8dc849276f679ae439c67ec529627d2379ee29ef
André Diogo <andrediogods@gmail.com>
2025-04-04 09:11:20 -0300

    Primeiro código em PHP.

---
+++ teste2.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+
+echo "OS dados Recebidos do Formulário HTML foram: ";
+echo "<br/>";
+echo "Nome: "   . $nome;
+echo "<br/>"; 
+echo "E-mail: " . $email;
